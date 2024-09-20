9411149a854993f823934e535fd1f2d507a9048b
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2024-09-19 21:25:52 -0400

    assignment 1 some changes

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,9 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+      <h2>
+        Dhruv Gorasiya
+      </h2>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
