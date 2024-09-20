b5246028e38aaa0343e33ec7172e4a0f5f16d05f
DhruvGorasiya <80987415+DhruvGorasiya@users.noreply.github.com>
2024-09-19 22:17:13 -0400

    Update TOC.tsx

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,7 +8,17 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
-         <li><a href="https://github.com/DhruvGorasiya/kanbas-react-web-app">GitHub Repository</a></li>
+{/*          <li><a href="https://github.com/DhruvGorasiya/kanbas-react-web-app">GitHub Repository</a></li> */}
+          <li>
+        <a
+          id="wd-github"
+          href="https://github.com/DhruvGorasiya/kanbas-react-web-app/tree/a1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub Repository
+        </a>
+      </li>
 
       </ul>
     );
