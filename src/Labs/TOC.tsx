56a3c60629840cdf8f86efbe56e403015add4ba4
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2024-09-28 17:57:51 -0400

    CSS

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,19 +1,33 @@
 import { Link } from "react-router-dom";
 
 export default function TOC() {
-    return (
-      <ul>
-        <li><Link to="/Labs">Labs</Link></li>
-        <li><Link to="/Labs/Lab1">Lab 1</Link></li>
-        <li><Link to="/Labs/Lab2">Lab 2</Link></li>
-        <li><Link to="/Labs/Lab3">Lab 3</Link></li>
-        <li><Link to="/Kanbas">Kanbas</Link></li>
-        <li>
-        <a id="wd-github" href="https://github.com/DhruvGorasiya/kanbas-react-web-app/tree/a1" target="_blank" rel="noopener noreferrer">
-        GitHub Repository
-      </a>
-        </li>
-      </ul>
-    );
-  }
-  
+  return (
+    <ul>
+      <li>
+        <Link to="/Labs">Labs</Link>
+      </li>
+      <li>
+        <Link to="/Labs/Lab1">Lab 1</Link>
+      </li>
+      <li>
+        <Link to="/Labs/Lab2">Lab 2</Link>
+      </li>
+      <li>
+        <Link to="/Labs/Lab3">Lab 3</Link>
+      </li>
+      <li>
+        <Link to="/Kanbas">Kanbas</Link>
+      </li>
+      <li>
+        <a
+          id="wd-github"
+          href="https://github.com/DhruvGorasiya/kanbas-react-web-app/tree/a1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub Repository
+        </a>
+      </li>
+    </ul>
+  );
+}
