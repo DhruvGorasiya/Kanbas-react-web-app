6af1bd549c9555f13fcadbe4d71654c70bd5fc6a
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2024-10-03 17:15:55 -0400

    HomeScreenUpdate

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,7 +1,8 @@
+import ModulesControls from "./ModulesControls";
+
 export default function Modules() {
   return (
     <div>
-      {/* Implement Collapse All button, View Progress button, etc. */}
       <button>Collapse All</button>
       <button>View Progress</button>
       <select id="wd-role">
@@ -12,77 +13,89 @@ export default function Modules() {
         <option value="STUDENT">Student</option>
       </select>
       <button>+ Module</button>
-      <ul id="wd-modules">
-        <li className="wd-module">
-          <div className="wd-title">Week 1</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to the course</li>
-                <li className="wd-content-item">
-                  Learn what is Web Development
-                </li>
-              </ul>
-            </li>
-            <li className="wd-lesson">
-              <span className="wd-title">READING</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  Full Stack Developer - Chapter 1 - Introduction
-                </li>
-                <li className="wd-content-item">
-                  Full Stack Developer - Chapter 2 - Creating Us
-                </li>
-              </ul>
-            </li>
-            <li className="wd-lesson">
-              <span className="wd-title">SLIDES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  Introduction to Web Development
-                </li>
-                <li className="wd-content-item">
-                  Creating an HTTP Server wih Node.js
-                </li>
-                <li className="wd-content-item">
-                  Creating a React Application
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li className="wd-module">
-          <div className="wd-title">Week 1, Lecture 2 - Formatting User Interfaces with HTML</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul>
-                <li className="wd-content-item">
-                  Learn how to create user interfaces with HTML
-                </li>
-                <li className="wd-content-item">
-                  Deploy the assignment to Netlify
-                </li>
-              </ul>
-            </li>
-            <li className="wd-lesson">
-              <span className="wd-title">SLIDES</span>
-              <ul>
-                <li className="wd-content-item">
-                  Introduction to HTML and the DOM
-                </li>
-                <li className="wd-content-item">
-                  Formatting Web content with Heading{" "}
-                </li>
-                <li className="wd-content-item">
-                  Formatting contenet with Lists and Tables
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-      </ul>
+
+      <div>
+        <ModulesControls />
+        <br />
+        <br />
+        <br />
+        <br />
+        <ul id="wd-modules" className="list-group rounded-0">
+          <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+            <div className="wd-title p-3 ps-2 bg-secondary"> Week 1 </div>
+            <ul className="wd-lessons list-group rounded-0">
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                LEARNING OBJECTIVES{" "}
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                Introduction to the course{" "}
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                Learn what is Web Development{" "}
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 1 </li>
+              <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 2 </li>
+            </ul>
+          </li>
+          
+
+          <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+            <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
+            <ul className="wd-lessons list-group rounded-0">
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                LEARNING OBJECTIVES{" "}
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                Learn how to create user interfaces with HTML{" "}
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                Deploy the assignment to Netlify{" "}
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                {" "}
+                Formatting Web content with Heading{" "}
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                {" "}
+                Formatting contenet with Lists and Tables{" "}
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 1 </li>
+              <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 2 </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   );
+
+
+  // return (
+  //   <div>
+  // <ModulesControls /><br /><br /><br /><br />
+  // <ul id="wd-modules" className="list-group rounded-0">
+  //   <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+  //     <div className="wd-title p-3 ps-2 bg-secondary"> Week 1 </div>
+  //     <ul className="wd-lessons list-group rounded-0">
+  //       <li className="wd-lesson list-group-item p-3 ps-1">
+  //         LEARNING OBJECTIVES </li>
+  //       <li className="wd-lesson list-group-item p-3 ps-1">
+  //         Introduction to the course </li>
+  //       <li className="wd-lesson list-group-item p-3 ps-1">
+  //         Learn what is Web Development </li>
+  //       <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 1 </li>
+  //       <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 2 </li>
+  //     </ul>
+  //   </li>
+  //   <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+  //     <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
+  //     <ul className="wd-lessons list-group rounded-0">
+  //       <li className="wd-lesson list-group-item p-3 ps-1">
+  //         LEARNING OBJECTIVES </li>
+  //       <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 1 </li>
+  //       <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 2 </li>
+  //     </ul>
+  //   </li>
+  // </ul> </div>
+
+  // );
 }
