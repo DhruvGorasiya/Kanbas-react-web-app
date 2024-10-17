7928c811af4e74541d84fb8ff449658043af325c
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2024-10-17 17:28:47 -0400

    navigation

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -3,10 +3,19 @@ import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 import { SlCalender } from "react-icons/sl";
-import { Link } from "react-router-dom";
 import { RiInbox2Fill } from "react-icons/ri";
 import { IoSettingsOutline } from "react-icons/io5";
+import { Link, useLocation } from "react-router-dom";
 export default function KanbasNavigation() {
+  const { pathname } = useLocation();
+  const links = [
+    { label: "Dashboard", path: "/Kanbas/Dashboard", icon: AiOutlineDashboard },
+    { label: "Courses",   path: "/Kanbas/Dashboard", icon: LiaBookSolid },
+    { label: "Calendar",  path: "/Kanbas/Calendar",  icon: IoCalendarOutline },
+    { label: "Inbox",     path: "/Kanbas/Inbox",     icon: FaInbox },
+    { label: "Labs",      path: "/Labs",             icon: LiaCogSolid },
+  ];
+
   return (
     <div
       id="wd-kanbas-navigation"
@@ -22,16 +31,23 @@ export default function KanbasNavigation() {
         <img src="/images/NEU.png" width="75px" />
       </a>
 
-      <Link
-        to="/Kanbas/Account"
-        id="wd-account-link"
-        className="list-group-item text-center border-0 bg-black text-white"
-      >
-        <FaRegCircleUser className="fs-1 text text-white" />
+      <Link to="/Kanbas/Account" className={`list-group-item text-center border-0 bg-black
+            ${pathname.includes("Account") ? "bg-white text-danger" : "bg-black text-white"}`}>
+        <FaRegCircleUser className={`fs-1 ${pathname.includes("Account") ? "text-danger" : "text-white"}`} />
         <br />
-        Account{" "}
+        Account
       </Link>
 
+      {links.map((link) => (
+        <Link key={link.path} to={link.path} className={`list-group-item bg-black text-center border-0
+              ${pathname.includes(link.label) ? "text-danger bg-white" : "text-white bg-black"}`}>
+          {link.icon({ className: "fs-1 text-danger"})}
+          <br />
+          {link.label}
+        </Link>
+      ))}
+
+{/* 
       <Link
         to="/Kanbas/Dashboard"
         id="wd-dashboard-link"
@@ -88,7 +104,7 @@ export default function KanbasNavigation() {
         <br />
         Labs{" "}
       </Link>
-      <br />
-    </div>
+      <br /> */}
+    </div> 
   );
 }
