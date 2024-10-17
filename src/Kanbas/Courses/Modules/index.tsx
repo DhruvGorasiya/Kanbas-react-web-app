7248c726308114d0ab7e518bfe3568137039b6d2
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2024-10-17 18:25:58 -0400

    3.7

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -2,8 +2,12 @@ import LessonControlButtons from "./LessonControlButtons";
 import ModuleControlButtons from "./ModuleControlButtons";
 import ModulesControls from "./ModulesControls";
 import { BsGripVertical } from "react-icons/bs";
+import { useParams } from "react-router";
+import * as db from "../../Database";
 
 export default function Modules() {
+  const { cid } = useParams();
+  const modules = db.modules;
   return (
     <div>
       <ModulesControls />
@@ -12,7 +16,7 @@ export default function Modules() {
       <br />
       <br />
       <ul id="wd-modules" className="list-group rounded-0">
-        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+        {/* <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
           <div className="wd-title p-3 ps-2 bg-secondary">
             {" "}
             <BsGripVertical className="me-2 fs-3" />
@@ -82,7 +86,25 @@ export default function Modules() {
             <li className="wd-lesson list-group-item p-3 ps-1"> <BsGripVertical className="me-2 fs-3" />
             LESSON 2<LessonControlButtons /> </li>
           </ul>
-        </li>
+        </li> */}
+        {modules
+          .filter((module: any) => module.course === cid)
+          .map((module: any) => (
+          <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+            <div className="wd-title p-3 ps-2 bg-secondary">
+              <BsGripVertical className="me-2 fs-3" /> {module.name} <ModuleControlButtons />
+            </div>
+            {module.lessons && (
+              <ul className="wd-lessons list-group rounded-0">
+                {module.lessons.map((lesson: any) => (
+                  <li className="wd-lesson list-group-item p-3 ps-1">
+                    <BsGripVertical className="me-2 fs-3" /> {lesson.name} <LessonControlButtons />
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
       </ul>
     </div>
   );
