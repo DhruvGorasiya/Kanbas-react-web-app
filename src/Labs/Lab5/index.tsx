1c25c06ee56f38f9bbfb8285c78731480c99de8a
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2024-11-15 02:19:57 -0500

    bahar

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -5,16 +5,17 @@ import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithArrays from "./WorkingWithArrays";
 import HttpClient from "./HttpClient";
 import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
-import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";  
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 
 export default function Lab5() {
+  console.log(REMOTE_SERVER);
   return (
     <div id="wd-lab5">
       <h2>Lab 5</h2>
       <div className="list-group">
-      <a href={`${REMOTE_SERVER}/lab5/welcome`} className="list-group-item">
+        <a href={`${REMOTE_SERVER}/lab5/welcome`} className="list-group-item">
           Welcome
         </a>
       </div>
