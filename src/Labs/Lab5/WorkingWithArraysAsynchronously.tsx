801b0dcb40d98244d5d3cd04e43d094d2933ddc8
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2024-11-14 20:45:43 -0500

    deletetodo

--- src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
+++ src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -18,6 +18,13 @@ export default function WorkingWithArraysAsynchronously() {
     const todos = await client.createTodo();
     setTodos(todos);
   };
+  const postTodo = async () => {
+    const newTodo = await client.postTodo({
+      title: "New Posted Todo",
+      completed: false,
+    });
+    setTodos([...todos, newTodo]);
+  };
 
   return (
     <div id="wd-asynchronous-arrays">
@@ -28,6 +35,11 @@ export default function WorkingWithArraysAsynchronously() {
           onClick={createTodo}
           className="text-success float-end fs-3"
           id="wd-create-todo"
+        />
+        <FaPlusCircle
+          onClick={postTodo}
+          className="text-primary float-end fs-3 me-3"
+          id="wd-post-todo"
         />
       </h4>
       <ul className="list-group">
